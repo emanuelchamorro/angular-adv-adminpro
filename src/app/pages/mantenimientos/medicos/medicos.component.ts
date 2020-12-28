9dbc87d1ae562791e103c830df43716c59216d54
emanuelchamorro <emanuel.chamorro@gmail.com>
2020-12-28 00:06:42 -0300

    commit

--- src/app/pages/mantenimientos/medicos/medicos.component.ts
+++ src/app/pages/mantenimientos/medicos/medicos.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { delay } from 'rxjs/operators';
 import { Medico } from 'src/app/models/medico.model';
+import { BusquedasService } from 'src/app/services/busquedas.service';
 import { MedicoService } from 'src/app/services/medico.service';
+import { ModalImagenService } from 'src/app/services/modal-imagen.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -8,16 +11,31 @@ import Swal from 'sweetalert2';
   templateUrl: './medicos.component.html',
   styles: []
 })
-export class MedicosComponent implements OnInit {
+export class MedicosComponent implements OnInit, OnDestroy {
 
    public medicos: Medico[] = [];
-   public cargando: boolean = true
+   public cargando: boolean = true;
+   public medicosTemp:Medico[]=[];
 
 
-  constructor(private medicoService:MedicoService) { }
+  constructor(private medicoService:MedicoService, 
+              private modalService:ModalImagenService,
+              private busqueda:BusquedasService) { }
+
+  ngOnDestroy(): void {
+    this.modalService.nuevaImagen.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.cargarMedicos();
+
+    //Para refrescar la vista una vez cambiada la imagen del hospital
+    this.modalService.nuevaImagen
+    .pipe(
+      //Para que no recarge antes de que el servidor haya terminado de subir la imagen
+      delay(1000)
+     )
+    .subscribe(img => this.cargarMedicos())
 
   }
 
@@ -29,5 +47,32 @@ cargarMedicos() {
 })
 }
  
+abrirModal(medico: Medico) {
+  this.modalService.abrirModal('medicos', medico._id, medico.img); 
+}
+
+buscar(termino:string) {
+  
+  if(termino.length === 0) {
+    return this.cargarMedicos();
+   
+  }
+  
+  this.busqueda.buscar('medicos', termino)
+     .subscribe((resp:Medico[]) => {
+       this.medicos = resp
+     })
+}
+
+borrarMedico(medico:Medico) {
+  this.medicoService.borrarMedicos(medico._id)
+                      .subscribe(resp => {
+                        this.cargarMedicos();
+                        Swal.fire('Borrado', medico.nombre, 'success');
+                      });
+}
+
+
+
 
 }
--- src/app/services/busquedas.service.ts
+++ src/app/services/busquedas.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Hospital } from '../models/hospital.model';
+import { Medico } from '../models/medico.model';
 import { Usuario } from '../models/usuario.model';
 
 const base_url= environment.base_url;
@@ -38,7 +39,11 @@ private transformarEnHospitales(resultados:any[]):Hospital[] {
    //No hace falta instanciar el hospital porque la imagen ya la muestro con un pipe
     return resultados;
     }
-  
+
+private transformarEnMedicos(resultados:any[]):Medico[] {
+      //No hace falta instanciar el hospital porque la imagen ya la muestro con un pipe
+       return resultados;
+       }  
 
 buscar(tipo: 'usuarios'|'medicos'|'hospitales', termino:string ) {
 
@@ -52,7 +57,10 @@ buscar(tipo: 'usuarios'|'medicos'|'hospitales', termino:string ) {
               return this.transformarEnUsuarios(resp.resultados);
             
               case 'hospitales':
-              return this.transformarEnHospitales(resp.resultados)
+              return this.transformarEnHospitales(resp.resultados);
+
+              case 'medicos':
+                return this.transformarEnMedicos(resp.resultados)
 
               default:
                 return [];
