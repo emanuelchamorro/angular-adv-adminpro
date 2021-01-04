1cc191b635c1b78a874bad1770780f121cfc8ced
Emanuel Chamorro <emanuel.chamorro@gmail.com>
2021-01-04 15:28:52 -0300

    medico fix

--- src/app/pages/mantenimientos/medico.component.ts
+++ src/app/pages/mantenimientos/medico.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Hospital } from 'src/app/models/hospital.model';
+import { HospitalService } from 'src/app/services/hospital.service';
 
 @Component({
   selector: 'app-medico',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicoComponent implements OnInit {
 
-  constructor() { }
+  public hospitales:Hospital[] = [];
+  public medicoForm:FormGroup;
+  public hospitalSeleccionado:Hospital
+
+  constructor(private fb:FormBuilder,
+              private hospitalService: HospitalService) { }
 
   ngOnInit(): void {
+
+    this.cargarHospitales()
+
+    this.medicoForm = this.fb.group({
+      nombre: ['Hernando', Validators.required],
+      hospital: ['', Validators.required]
+    })
+
+ //Para lograr que traiga la info del hospital cuando lo selecciono en el select
+    this.medicoForm.get('hospital').valueChanges.subscribe(
+         hospitalId=> {
+           this.hospitalSeleccionado = this.hospitales.find(h => h._id === hospitalId );
+           console.log(this.hospitalSeleccionado)
+         }
+    )
+
+
+  }
+
+
+
+
+  cargarHospitales(){
+    this.hospitalService.cargarHospitales().subscribe((hospital:Hospital[]) => {
+      this.hospitales = hospital;
+ 
+    })
+  }
+
+
+
+  guardarMedico(){
+    console.log(this.medicoForm.value)
   }
 
 }
